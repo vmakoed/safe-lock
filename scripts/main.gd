d518b764606c11c23adebaea14bdbcd50cb90fcf
Viktar Makayed <vmakoed@gmail.com>
2025-11-08 10:41:30 +0100

    Implement Maze level

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,14 +3,9 @@ extends Node2D
 @onready var level_selector := %LevelSelector
 @onready var tic_tac_toe := %TicTacToe
 @onready var safe := %Safe
+@onready var maze := %Maze
 
 var current_level: Control = null
-
-func _ready() -> void:
-	pass
-
-func _process(_delta: float) -> void:
-	pass
 
 func _on_level_back_button_pressed() -> void:
 	current_level.visible = false
@@ -21,6 +16,9 @@ func _on_tic_tac_toe_level_button_pressed() -> void:
 
 func _on_safe_level_button_pressed() -> void:
 	_switch_level(safe)
+
+func _on_maze_level_button_pressed() -> void:
+	_switch_level(maze)
 
 func _switch_level(level) -> void:
 	current_level = level
---
+++ scripts/maze.gd
@@ -0,0 +1,32 @@
+extends Control
+
+const GRID_SIZE = 64
+
+@onready var player := %Player
+@onready var maze_solution := %MazeSolutionTileMap
+
+func _on_up_button_pressed() -> void:
+	_move(Vector2.UP)
+
+func _on_left_button_pressed() -> void:
+	_move(Vector2.LEFT)
+
+func _on_right_button_pressed() -> void:
+	_move(Vector2.RIGHT)
+
+func _on_down_button_pressed() -> void:
+	_move(Vector2.DOWN)
+
+func _move(direction: Vector2) -> void:
+	var position_change := direction * GRID_SIZE
+	var motion = Vector2(position_change)
+	var hit = player.move_and_collide(motion, true)
+
+	if hit == null:
+		player.position += position_change
+
+
+func _on_exit_body_entered(body: Node2D) -> void:
+	if body == player:
+		maze_solution.visible = true
+		player.visible = false
