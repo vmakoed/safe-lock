a5575ffb60b31262644f10e00df3bd044c729e7c
Viktar Makayed <vmakoed@gmail.com>
2025-11-17 14:34:15 +0100

    Extract Maze scene

--- scripts/maze.gd
+++ scripts/maze.gd
@@ -2,8 +2,10 @@ extends Control
 
 const GRID_SIZE = 64
 
+signal back_button_pressed
+
 @onready var player := %Player
-@onready var maze_solution := %MazeSolutionTileMap
+@onready var solution := %SolutionTileMap
 
 func _on_up_button_pressed() -> void:
 	_move(Vector2.UP)
@@ -12,6 +14,7 @@ func _on_left_button_pressed() -> void:
 	_move(Vector2.LEFT)
 
 func _on_right_button_pressed() -> void:
+	print("pressed")
 	_move(Vector2.RIGHT)
 
 func _on_down_button_pressed() -> void:
@@ -25,8 +28,10 @@ func _move(direction: Vector2) -> void:
 	if hit == null:
 		player.position += position_change
 
-
 func _on_exit_body_entered(body: Node2D) -> void:
 	if body == player:
-		maze_solution.visible = true
+		solution.visible = true
 		player.visible = false
+
+func _on_back_button_pressed() -> void:
+	back_button_pressed.emit()
