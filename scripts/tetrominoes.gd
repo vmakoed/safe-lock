d11732c307257e4f4bfa393b705376eb4c22a553
Viktar Makayed <vmakoed@gmail.com>
2025-11-17 14:40:07 +0100

    Extract Tetrominoes scene

--- scripts/tetrominoes.gd
+++ scripts/tetrominoes.gd
@@ -1,8 +1,10 @@
 extends Control
 
-@onready var tetrominoes_board := %TetrominoesBoard
-@onready var tetrominoes_controls := %TetrominoesControls
-@onready var tetrominoes_buttons := %TetrominoesButtons
+signal back_button_pressed
+
+@onready var board := %Board
+@onready var controls := %Controls
+@onready var buttons := %Buttons
 
 @export var cell_size: Vector2i = Vector2i(128, 128)
 @export var grid_size: Vector2i = Vector2i(5, 4)
@@ -10,12 +12,12 @@ extends Control
 var active_piece: Control = null
 
 func _ready() -> void:
-	var up_button = tetrominoes_controls.get_node("UpButton")
-	var left_button = tetrominoes_controls.get_node("LeftButton")
-	var right_button = tetrominoes_controls.get_node("RightButton")
-	var down_button = tetrominoes_controls.get_node("DownButton")
-	var confirm_button = tetrominoes_controls.get_node("ConfirmButton")
-	var delete_button = tetrominoes_controls.get_node("DeleteButton")
+	var up_button = controls.get_node("UpButton")
+	var left_button = controls.get_node("LeftButton")
+	var right_button = controls.get_node("RightButton")
+	var down_button = controls.get_node("DownButton")
+	var confirm_button = controls.get_node("ConfirmButton")
+	var delete_button = controls.get_node("DeleteButton")
 
 	up_button.pressed.connect(_on_up_button_pressed)
 	left_button.pressed.connect(_on_left_button_pressed)
@@ -24,8 +26,8 @@ func _ready() -> void:
 	confirm_button.pressed.connect(_on_confirm_button_pressed)
 	delete_button.pressed.connect(_on_delete_button_pressed)
 
-	for i in range(tetrominoes_buttons.get_child_count()):
-		var button = tetrominoes_buttons.get_child(i)
+	for i in range(buttons.get_child_count()):
+		var button = buttons.get_child(i)
 		button.pressed.connect(set_active_piece.bind(i))
 
 
@@ -57,29 +59,32 @@ func _on_delete_button_pressed() -> void:
 	_on_confirm_button_pressed()
 
 func set_active_piece(piece_index: int) -> void:
-	var piece = tetrominoes_board.get_child(piece_index)
+	var piece = board.get_child(piece_index)
 	active_piece = piece
 	active_piece.visible = true
 	_disable_piece_buttons()
 	_fade_out_board()
 
 func _disable_piece_buttons() -> void:
-	for i in range(tetrominoes_buttons.get_child_count()):
-		var button = tetrominoes_buttons.get_child(i)
+	for i in range(buttons.get_child_count()):
+		var button = buttons.get_child(i)
 		button.disabled = true
 
 func _enable_piece_buttons() -> void:
-	for i in range(tetrominoes_buttons.get_child_count()):
-		var button = tetrominoes_buttons.get_child(i)
+	for i in range(buttons.get_child_count()):
+		var button = buttons.get_child(i)
 		button.disabled = false
 
 func _fade_out_board() -> void:
-	for i in range(tetrominoes_board.get_child_count()):
-		var piece = tetrominoes_board.get_child(i)
+	for i in range(board.get_child_count()):
+		var piece = board.get_child(i)
 		if piece != active_piece:
 			piece.modulate = Color(0, 0, 0, 0.5)
 
 func _fade_in_board() -> void:
-	for i in range(tetrominoes_board.get_child_count()):
-		var piece = tetrominoes_board.get_child(i)
+	for i in range(board.get_child_count()):
+		var piece = board.get_child(i)
 		piece.modulate = Color(1, 1, 1, 1)
+
+func _on_back_button_pressed() -> void:
+	back_button_pressed.emit()
